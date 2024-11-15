eeba52d3dd9dc525c186bf1b840e1ec220037ffb
Jason Fan <fanjas112358@gmail.com>
2024-11-14 20:49:35 -0500

    actual working resize + center
    this idea should be scalable and if it isn't I'm fucked

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-const imgw = 500;
-const imgh = 400;
+// not the actual size of the image, these variables just have to be
+// representative of the aspect ratio
+const imgw = 5;
+const imgh = 4;
 
 function resized()
 {
-    let mapimg = document.getElementById("mapimg");
-    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    let active = document.getElementById("active");
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-    mapimg.style.width = (Math.floor(vw / imgw) * imgw).toString + "px";
-    mapimg.style.height = (Math.floor(vw / imgh) * imgh).toString + "px";
-
-    console.log(vw);
-    console.log(Math.floor(vw / imgw) * imgw);
+    active.style.width = (Math.min(vw / imgw, vh / imgh) * imgw).toString() + "px";
+    active.style.height = (Math.min(vw / imgw, vh / imgh) * imgh).toString() + "px";
 }
 
 addEventListener("resize", (event) => {resized();})
