19119f10009d27221c95c039d8582c402839e3ed
Cuboid <fanjas112358@gmail.com>
2024-12-27 20:55:38 -0500

    hotel des invalides and palais garnier
    also capped the map size at 0.9x viewport cuz idk just feels less goofy

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ for (let i = 0; i < iconelements.length; i++) {
 function resized(event)
 {
     let active = document.getElementById("active");
-    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9;
+    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) * 0.9;
 
     active.style.width = (Math.min(vw / imgw, vh / imgh) * imgw).toString() + "px";
     active.style.height = (Math.min(vw / imgw, vh / imgh) * imgh).toString() + "px";
