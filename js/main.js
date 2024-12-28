47fe293879f59a63f911acfc62dd284147769902
Cuboid <fanjas112358@gmail.com>
2024-12-27 21:31:28 -0500

    the funny css make the funny pop go brrr
    also moved resized() but who cares

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@
 const imgw = 5;
 const imgh = 4;
 
+function resized(event)
+{
+    let active = document.getElementById("active");
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9;
+    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) * 0.9;
+
+    active.style.width = (Math.min(vw / imgw, vh / imgh) * imgw).toString() + "px";
+    active.style.height = (Math.min(vw / imgw, vh / imgh) * imgh).toString() + "px";
+}
+
 let iconelements = document.getElementsByClassName("icon");
 
 for (let i = 0; i < iconelements.length; i++) {
@@ -13,16 +23,6 @@ for (let i = 0; i < iconelements.length; i++) {
    arrowimg.classList.add("arrow");
 
    elem.appendChild(arrowimg);
-}
-
-function resized(event)
-{
-    let active = document.getElementById("active");
-    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9;
-    let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) * 0.9;
-
-    active.style.width = (Math.min(vw / imgw, vh / imgh) * imgw).toString() + "px";
-    active.style.height = (Math.min(vw / imgw, vh / imgh) * imgh).toString() + "px";
 }
 
 addEventListener("resize", resized);
