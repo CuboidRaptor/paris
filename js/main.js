1b61ed9195d05d4845a8de6d8d784d555a4d5f96
Cuboid <fanjas112358@gmail.com>
2025-01-12 20:19:10 -0500

    styling!
    titles are displayed properly now
    larger sidebar (20%->25%)
    fonts
    removing redundant code

--- js/main.js
+++ js/main.js
@@ -7,20 +7,39 @@ function resized(event)
 {
     let active = document.getElementById("active");
     // 0.85 is 1-15%, from sidebar css
-    let vw = Math.max((document.documentElement.clientWidth * 0.85) || 0, (window.innerWidth * 0.85) || 0) * 0.9;
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9;
     let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) * 0.9;
 
     active.style.width = (Math.min(vw / imgw, vh / imgh) * imgw).toString() + "px";
     active.style.height = (Math.min(vw / imgw, vh / imgh) * imgh).toString() + "px";
 }
 
+const idmap = {
+    eiffel: "La Tour Eiffel",
+    triomphe: "L'Arc de Triomphe de l'Étoile",
+    coeur: "La Basilique du Sacré-Cœur",
+    notredame: "La Cathédrale Notre-Dame de Paris",
+    moulin: "Le Moulin-Rouge",
+    invalides: "L'Hôtel des Invalides",
+    palais: "Le Palais Garnier",
+    pantheon: "Le Panthéon de Paris",
+    lyon: "La Gare de Lyon"
+}
+
 function clicked(event) {
-    console.log(event.currentTarget.id);
     let sidebar = document.getElementById("sidebar");
+    let title = document.getElementById("sbtitle");
 
     sidebar.classList.remove("righttransition");
-    sidebar.style.right = "-20%";
+    sidebar.style.right = "-25%";
     document.body.offsetHeight; // chat I know this looks fucked but it's to trigger a reflow...
+
+    let newtitle = idmap[event.currentTarget.id];
+    if (newtitle === undefined) {
+        throw new Error("CSS ID nonexistent in idmap");
+    }
+    title.innerHTML = newtitle;
+
     sidebar.classList.add("righttransition"); // <-- ...so this stuff actually transitions
     sidebar.style.right = "0px";
 }
