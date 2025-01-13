de02ac6c3854dcfab9532aa79eb58d5e9cafd7f0
Cuboid <fanjas112358@gmail.com>
2025-01-13 16:29:45 -0500

    I am the stupido
    i did this with urls last and I just forgot how to think

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 // backslashes escape line endings, so though there's a newline in the code, the newlien will not be stored/displayed.
 // for clarity's sake, as scrolling through long code sucks shit.
-// no indent after line break!!!
+// no indent after line break!!! (add a space if there was one before)
 gdata = {
     "idtitles": {
         eiffel: "La Tour Eiffel",
@@ -14,17 +14,17 @@ gdata = {
         lyon: "La Gare de Lyon"
     },
     "blurbs": {
-        eiffel: `C'est le premier monument de Paris. Si vous allez en France,\
+        eiffel: `C'est le premier monument de Paris. Si vous allez en France, \
 vous ne pourrez pas sortir avant de visiter la tour Eiffel.`,
-        triomphe: `Un symbole de la révolution français et des guerres napoléoniennes.\
+        triomphe: `Un symbole de la révolution français et des guerres napoléoniennes. \
 Il honore les soldats tués dans ces guerres et c’est un monument très important de Paris.`,
         coeur: `La basilique du Sacré-Cœur de Montmartre est une église qui est consacrée au cœur de Jésus.`,
-        notredame: `Beaucoup de personnes connaissent Notre-Dame, qui est une cathédrale très historique et grande.\
+        notredame: `Beaucoup de personnes connaissent Notre-Dame, qui est une cathédrale très historique et grande. \
 Il a plus d'art inestimable.`,
         moulin: ``, // no
         invalides: `C'est un complexe de beaucoup de bâtiments et de cours. Il est devenu un musée et une tombe.`,
         palais: `Le Palais Garnier, aussi connu sous le nom de l'Opéra Garnier, est un plus grand théâtre et une académie des arts.`,
-        pantheon: `Le Panthéon est un monument situé dans le 5ᵉ arrondissement de Paris.\
+        pantheon: `Le Panthéon est un monument situé dans le 5ᵉ arrondissement de Paris. \
 Il a été une église, puis il est devenu un mausolée et un musée pour les personnes importantes de Paris.`,
         lyon: `` // no
     }
