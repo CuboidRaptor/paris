75e8ee6e71a827ff89177dd9ff6a3e182a35b67b
Jason Fan <fanjas112358@gmail.com>
2025-01-13 09:43:52 -0500

    l i n e

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ gdata = {
     "blurbs": {
         eiffel: `C'est le premier monument de Paris. Si vous allez en France, vous ne pourrez pas sortir avant de visiter la tour Eiffel.`,
         triomphe: `Un symbole de la révolution français et des guerres napoléoniennes. Il honore les soldats tués dans ces guerres et c’est un monument très important de Paris.`,
-        coeur: `Une église qui est consacrée au cœur de Jésus.`,
+        coeur: `La basilique du Sacré-Cœur de Montmartre est une église qui est consacrée au cœur de Jésus.`,
         notredame: `Une cathédrale très historique et grande. Il a plus d'art inestimable.`,
         moulin: ``,
         invalides: `C'est un complexe de beaucoup des bâtiments et des cours. Il est devenu un musée et une tombe.`,
